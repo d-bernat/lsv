cff837751d2d2ae0e0bb7f024f0d4923f4192ecd
bernat <bernat@traveltainment.de>
2017-09-28 23:10:55 +0200

    fix

---
+++ public/app/directives/routeLoadingIndicator.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('routeLoadingIndicator', [])
+    .directive('routeLoadingIndicator', ['$rootScope', function ($rootScope) {
+        return {
+            restrict: 'E',
+            template:"<div ng-if='isRouteLoading' id='escapingBallG'><div id='escapingBall_1' class='escapingBallG'></div></div>",
+            link:function(scope, elem, attrs) {
+                scope.isRouteLoading = false;
+
+                $rootScope.$on('$routeChangeStart', function () {
+                    scope.isRouteLoading = true;
+                    console.log(scope.isRouteLoading);
+                });
+
+                $rootScope.$on('$routeChangeSuccess', function () {
+                    scope.isRouteLoading = false;
+                    console.log(scope.isRouteLoading);
+                });
+            }
+        }
+    }]);
